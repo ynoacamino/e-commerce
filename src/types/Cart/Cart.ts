7cc449c7e3efb9115ea0578e80eca58d0e907b9d
Diegoo11 <ynoacamino@gmail.com>
2024-03-14 13:53:05 -0500

    Remove user_id from NewCart interface

--- src/types/Cart/Cart.ts
+++ src/types/Cart/Cart.ts
@@ -1,7 +1,6 @@
 export interface NewCart {
   cart_count: number;
   product_id: number;
-  user_id: number;
 }
 
 export interface UpdateCart extends NewCart {}
--- src/types/Cart/ParseCart.ts
+++ src/types/Cart/ParseCart.ts
@@ -1,18 +1,16 @@
 import { isNumber } from '@/lib/parserTypes';
 import { NewCart, UpdateCart } from './Cart';
 
-export const isNewCart = (newCart: any): newCart is NewCart => {
+export const isNewCart = (newCart: unknown): newCart is NewCart => {
   if (
     !!newCart
     && typeof newCart === 'object'
     && 'cart_count' in newCart
     && 'product_id' in newCart
-    && 'user_id' in newCart
   ) {
     return (
       isNumber(newCart.cart_count)
       && isNumber(newCart.product_id)
-      && isNumber(newCart.user_id)
     );
   }
   return false;
